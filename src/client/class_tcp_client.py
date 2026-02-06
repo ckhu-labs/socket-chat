af80ecfc808dac79a91a24c1c00fef4ad838d6e0
ckhu-labs <khucharlie@gmail.com>
2026-02-06 12:02:50 -0700

    in class tcp client and server

---
+++ src/client/class_tcp_client.py
@@ -0,0 +1,14 @@
+from socket import *
+serverName = "10.187.201.253"
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+sentence = input('Enter a sentence: ')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+
+print('From Server:', modifiedSentence.decode())
+
+clientSocket.close()
